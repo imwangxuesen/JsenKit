0c03fae9556c2500ac86aec7ffb4f8ecaf81eb9f
Jsoon <imwangxuesen@icloud.com>
2018-03-10 16:11:37 +0800

    add UIColor function

--- JsenKit/JsenCategory/UIColor+JsenKit.h
+++ JsenKit/JsenCategory/UIColor+JsenKit.h
@@ -18,4 +18,13 @@
  @return UIColor
  */
 + (UIColor*)js_ColorWithHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue;
+
+/**
+ Hex Color
+
+ @param hexString 16进制字符串
+ @param alpha 透明度
+ @return UIColor
+ */
++ (instancetype)js_colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha;
 @end
--- JsenKit/JsenCategory/UIColor+JsenKit.m
+++ JsenKit/JsenCategory/UIColor+JsenKit.m
@@ -15,4 +15,29 @@
                            green:((float)((hexValue & 0xFF00) >> 8))/255.0
                             blue:((float)(hexValue & 0xFF))/255.0 alpha:alphaValue];
 }
+
++ (instancetype)js_colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha{
+    
+    hexString = [[hexString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    UIColor *defaultColor = [UIColor clearColor];
+    
+    if (hexString.length < 6) return defaultColor;
+    if ([hexString hasPrefix:@"#"]) hexString = [hexString substringFromIndex:1];
+    if ([hexString hasPrefix:@"0X"]) hexString = [hexString substringFromIndex:2];
+    if (hexString.length != 6) return defaultColor;
+    
+    //method1
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    unsigned int hexNumber;
+    if (![scanner scanHexInt:&hexNumber]) return defaultColor;
+    
+    //method2
+    const char *char_str = [hexString cStringUsingEncoding:NSASCIIStringEncoding];
+    int hexNum;
+    sscanf(char_str, "%x", &hexNum);
+    
+    return [UIColor js_ColorWithHex:hexNumber alpha:alpha];
+}
+
 @end
