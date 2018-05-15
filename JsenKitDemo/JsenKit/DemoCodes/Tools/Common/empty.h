c8c6a40b6ba728fc05fa35e3866d2d38355cb71c
Jsoon <imwangxuesen@icloud.com>
2018-05-15 17:46:23 +0800

    fix ignore

---
+++ JsenKitDemo/JsenKit/DemoCodes/Tools/Common/empty.h
@@ -0,0 +1,13 @@
+//
+//  empty.h
+//  JsenKit
+//
+//  Created by WangXuesen on 2018/5/15.
+//  Copyright © 2018年 WangXuesen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface empty : NSObject
+
+@end
---
+++ JsenKitDemo/JsenKit/DemoCodes/Tools/Common/empty.m
@@ -0,0 +1,13 @@
+//
+//  empty.m
+//  JsenKit
+//
+//  Created by WangXuesen on 2018/5/15.
+//  Copyright © 2018年 WangXuesen. All rights reserved.
+//
+
+#import "empty.h"
+
+@implementation empty
+
+@end
